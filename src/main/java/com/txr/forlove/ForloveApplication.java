d47d798d0ff8dc7be8628edbf20b2d6c1157c9f5
tangxi <406405466@qq.com>
2018-11-25 22:00:31 +0800

    init

---
+++ src/main/java/com/txr/forlove/ForloveApplication.java
@@ -0,0 +1,12 @@
+package com.txr.forlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForloveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForloveApplication.class, args);
+    }
+}
